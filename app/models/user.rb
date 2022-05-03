76028909e4b0004f0a08e0eec8a928c2b5d5ff55
Bruno Silveira <djbrunosilveira@gmail.com>
2022-05-02 21:32:50 -0300

    Fixes should_send_inbox method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,19 @@ class User < ApplicationRecord
   after_create :create_preference
 
   def create_preference
-    User::Preference.create(
+    preference = User::Preference.create(
       user: self,
       period: 'daily',
       at: '12:00'
     )
+
+    user.update(next_inbox_at: preference.next_inbox_at)
   end
 
   def should_send_newsletter?
-    return false
+    return false if next_inbox_at.nil?
+    return true if last_inbox_at.nil?
 
-    #Time.zone.now > next_inbox_at && next_inbox_at > last_inbox_at
+    Time.zone.now > next_inbox_at && next_inbox_at > last_inbox_at
   end
 end
