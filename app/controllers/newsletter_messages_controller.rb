5402c6f0562b9d54f5c0b9bdaf0e16d5272de074
Bruno Silveira <djbrunosilveira@gmail.com>
2022-05-02 21:27:47 -0300

    Opens newsletter message in new link

--- app/controllers/newsletter_messages_controller.rb
+++ app/controllers/newsletter_messages_controller.rb
@@ -1,10 +1,10 @@
 class NewsletterMessagesController < ApplicationController
   def show
-    @newsletter_message = NewsletterMessage.find(params[:id])
+    @newsletter_message = NewsletterMessage.find_by!(id: params[:id], user: current_user)
   end
 
   def body
-    @newsletter_message = NewsletterMessage.find(params[:id])
+    @newsletter_message = NewsletterMessage.find_by!(id: params[:id], user: current_user)
 
     respond_to do |format|
       format.html {  render inline: @newsletter_message.body }
