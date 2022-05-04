dd9019122e5b8b41b6788ce72b7d9748ef10420e
Bruno Silveira <djbrunosilveira@gmail.com>
2022-05-03 21:08:08 -0300

    Fixes mailer

--- app/mailers/inbox_mailer.rb
+++ app/mailers/inbox_mailer.rb
@@ -1,7 +1,7 @@
 class InboxMailer < ApplicationMailer
-  def inbox_email(user, newsletter_messages)
-    @user = user
-    @newsletter_messages = newsletter_messages
+  def inbox_email
+    @user = params[:user]
+    @newsletter_messages = params[:newsletter_messages]
 
     mail(to: @user.email, subject: "Here is your #{@user.preference.period} mailpouch!")
   end
