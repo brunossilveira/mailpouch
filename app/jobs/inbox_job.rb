6ceae33f4ce0138ebd573562bdd18ee5cb2bf77b
Bruno Silveira <djbrunosilveira@gmail.com>
2022-05-03 21:21:00 -0300

    Changes mailer

--- app/jobs/inbox_job.rb
+++ app/jobs/inbox_job.rb
@@ -3,12 +3,7 @@ class InboxJob < ApplicationJob
     User.find_each do |user|
       return if !user.should_send_newsletter?
 
-      newsletter_messages = NewsletterMessage.where(user: user).where(created_at: (user.last_inbox_at..user.next_inbox_at)).order(created_at: :desc)
-
-      if newsletter_messages.count > 0
-        user.update(last_inbox_at: Time.zone.now, next_inbox_at: user.preference.next_inbox_at)
-        InboxMailer.with(user: user, newsletter_messages: newsletter_messages.to_json).inbox_email.deliver_later
-      end
+      InboxMailer.with(user: user).inbox_email.deliver_later
     end
   end
 end
--- app/mailers/inbox_mailer.rb
+++ app/mailers/inbox_mailer.rb
@@ -1,8 +1,12 @@
 class InboxMailer < ApplicationMailer
   def inbox_email
     @user = params[:user]
-    @newsletter_messages = params[:newsletter_messages]
+    @newsletter_messages = NewsletterMessage.where(user: @user).where(created_at: (@user.last_inbox_at..@user.next_inbox_at)).order(created_at: :desc)
 
-    mail(to: @user.email, subject: "Here is your #{@user.preference.period} mailpouch!")
+    if @newsletter_messages.count > 0
+      @user.update(last_inbox_at: Time.zone.now, next_inbox_at: user.preference.next_inbox_at)
+
+      mail(to: @user.email, subject: "Here is your #{@user.preference.period} mailpouch!")
+    end
   end
 end
