3b0994110abd0a20a837acc4b453bffd4b1e3117
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-07 12:33:49 -0300

    Fixes smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailbox.ingress = :sendgrid
   config.active_storage.service = :amazon
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
     domain: ENV['DOMAIN'],
