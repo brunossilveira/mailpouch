0eb07e66bb9829f5680bb67f111174989a701bdd
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-13 20:30:19 -0300

    Test inbox job

--- app/jobs/inbox_job.rb
+++ app/jobs/inbox_job.rb
@@ -1,7 +1,7 @@
 class InboxJob < ApplicationJob
   def perform
     User.find_each do |user|
-      return if !user.should_send_newsletter?
+      next if !user.should_send_newsletter?
       Event.create(user: user, name: "inbox_job - should_send_newsletter: #{user.should_send_newsletter?}")
 
       InboxMailer.with(user: user).inbox_email.deliver_later
--- spec/jobs/inbox_job_spec.rb
+++ spec/jobs/inbox_job_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'InboxJob', type: :job do
   it "sends email when it should" do
-    user = instance_double('User', should_send_newsletter?: false)
-    InboxJob.perform_now
+    User.create(email: 'test@email.com', password: 'password', next_inbox_at: 5.minutes.ago)
+    User.create(email: 'test@email.com', password: 'password', next_inbox_at: nil)
+    User.create(email: 'test@email.com', password: 'password', next_inbox_at: nil)
+
+    InboxJob.new.perform
+
+    expect(Event.count).to eq(1)
   end
 end
