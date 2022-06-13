9dcc77e3e0a0f7e0298f83ec16f54e56b94d1833
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-13 20:13:33 -0300

    Adds event to inbox job

--- app/jobs/inbox_job.rb
+++ app/jobs/inbox_job.rb
@@ -2,6 +2,7 @@ class InboxJob < ApplicationJob
   def perform
     User.find_each do |user|
       return if !user.should_send_newsletter?
+      Event.create(user: user, name: "inbox_job - should_send_newsletter: #{user.should_send_newsletter?}")
 
       InboxMailer.with(user: user).inbox_email.deliver_later
     end
