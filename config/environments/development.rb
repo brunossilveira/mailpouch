810cacc6c92c0ca18e83e66cf6a215529617f523
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-13 20:46:02 -0300

    Fixes mailer preview path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << /[a-z0-9-]+\.ngrok\.io/
+  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 end
