afaea79ed5f41c3273058919e769b0dce1ad5097
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-13 22:07:42 -0300

    Potential fix worker

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,12 +29,12 @@ class User < ApplicationRecord
 
     newsletter_messages = NewsletterMessage.where(user: self).where(created_at: (last_inbox_at..next_inbox_at)).order(created_at: :desc)
 
-    return if newsletter_messages.count.zero?
+    if !newsletter_messages.count.zero?
+      InboxMailer.with(user: self, newsletter_messages: newsletter_messages).inbox_email.deliver_now
 
-    InboxMailer.with(user: self, newsletter_messages: newsletter_messages).inbox_email.deliver_now
+      Event.create(user: self, name: 'send_inbox', description: "Count: #{newsletter_messages.count} | Ids: #{newsletter_messages.ids}")
+    end
 
     update(last_inbox_at: Time.zone.now, next_inbox_at: preference.next_inbox_at)
-
-    Event.create(user: self, name: 'send_inbox', description: "Count: #{newsletter_messages.count} | Ids: #{newsletter_messages.ids}")
   end
 end
