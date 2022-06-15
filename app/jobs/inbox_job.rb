9a7f8acc3d5f82c9a2b1399e3cca811d95dfaebd
Bruno Silveira <djbrunosilveira@gmail.com>
2022-06-15 19:11:09 -0300

    Adds user to event creation so it doesnt blow up

--- app/jobs/inbox_job.rb
+++ app/jobs/inbox_job.rb
@@ -1,11 +1,11 @@
 class InboxJob < ApplicationJob
   def perform
-    Event.create(user: nil, name: 'started_inbox_job')
+    Event.create(user: User.first, name: 'started_inbox_job')
 
     User.all.each do |user|
       user.send_inbox
     end
 
-    Event.create(user: nil, name: 'finished_inbox_job')
+    Event.create(user: User.first, name: 'finished_inbox_job')
   end
 end
--- lib/tasks/run_inbox.rake
+++ lib/tasks/run_inbox.rake
@@ -1,8 +1,8 @@
 desc "Runs inbox"
 task run_inbox: :environment do
-  Event.create(user: nil, name: 'started_inbox_task')
+  Event.create(user: User.first, name: 'started_inbox_task')
 
   InboxJob.perform_later
 
-  Event.create(user: nil, name: 'finished_inbox_task')
+  Event.create(user: User.first, name: 'finished_inbox_task')
 end
