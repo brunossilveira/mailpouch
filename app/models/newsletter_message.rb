d66d2b16bb593c2cc959937bffed194da7c345b2
Bruno Silveira <djbrunosilveira@gmail.com>
2022-11-17 21:44:00 -0300

    Fixes typo

--- app/models/newsletter_message.rb
+++ app/models/newsletter_message.rb
@@ -6,6 +6,6 @@ class NewsletterMessage < ApplicationRecord
   scope :unread, -> { where(read: false) }
 
   def self.inbox_messages_by(user:)
-    NewsletterMessage.where(user: user).unread.where(created_at: (user.last_inbox_at..Time.zone.now).order(created_at: :desc)
+    NewsletterMessage.where(user: user).unread.where(created_at: (user.last_inbox_at..Time.zone.now)).order(created_at: :desc)
   end
 end
