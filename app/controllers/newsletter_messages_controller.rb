5f5cc64dffff989f9a6397549265ea18497f0d6a
Bruno Silveira <djbrunosilveira@gmail.com>
2022-11-22 22:27:17 -0300

    Loofah

--- app/controllers/newsletter_messages_controller.rb
+++ app/controllers/newsletter_messages_controller.rb
@@ -5,6 +5,16 @@ class NewsletterMessagesController < ApplicationController
   def show
     @newsletter_message = NewsletterMessage.find_by!(id: params[:id], user: current_user)
     @newsletter_message.update(read: true)
+
+    # ActionView::Base.full_sanitizer.sanitize(@newsletter_message.body, tags: %w(img br p span table tr td tbody))
+
+    scrub = Loofah::Scrubber.new do |node|
+      node.name = "p" if node.name == "td"
+      node.name = "div" if node.name == "table"
+      node.name = "div" if node.name == "tbody"
+    end
+
+    @sanitized_body = Loofah.fragment(@newsletter_message.body).scrub!(scrub).to_s
   end
 
   def read
