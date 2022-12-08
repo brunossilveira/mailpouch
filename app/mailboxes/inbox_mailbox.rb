0c9c32e05e8c614688e4ea5abb9e2c73ba3ebad0
Bruno Silveira <djbrunosilveira@gmail.com>
2022-12-08 18:55:01 -0300

    A bit of refactoring love

--- app/mailboxes/inbox_mailbox.rb
+++ app/mailboxes/inbox_mailbox.rb
@@ -4,8 +4,7 @@ class InboxMailbox < ApplicationMailbox
     user = User.find_by(username: username)
 
     if user
-      body = mail.html_part.body
-      body_text = mail.text_part.body
+      body = Mail::Body.new(mail)
 
       subject = mail.subject
       unsubscribe_header = mail.header['List-Unsubscribe']
@@ -15,29 +14,16 @@ class InboxMailbox < ApplicationMailbox
       unsubscribe_url = unsubscribe_header.field.value.split(',').first.delete('<').delete('>') if unsubscribe_header
       newsletter = Newsletter.find_by(email: domain, user: user) || Newsletter.find_or_create_by(email: domain, name: newsletter_name, user: user)
 
-      NewsletterMessage.create(user: user, newsletter: newsletter, subject: mail.subject, body: body, body_parsed: body_parsed(body), body_raw: body_raw(body), body_text: body_text, unsubscribe_url: unsubscribe_url)
+      NewsletterMessage.create(
+        user: user,
+        newsletter: newsletter,
+        subject: mail.subject,
+        body: body.unparsed,
+        body_parsed: body.parsed,
+        body_raw: body.raw,
+        body_text: body.text,
+        unsubscribe_url: unsubscribe_url
+      )
     end
-  end
-
-  def body_parsed(body)
-    options = {
-      elements: %w[a p strong ul li ol img br b hr h1 h2 h3 h4],
-      attributes: {
-        'a' => %w[href title],
-        'img' => %w[alt src title width height]
-      },
-      protocols: {
-        'a' => {'href' => ['http', 'https', 'mailto']}
-      },
-      add_attributes: {
-        'a' => {'rel' => 'nofollow'}
-      }
-    }
-
-    ::Sanitize.fragment(body, options)
-  end
-
-  def body_raw(body)
-    ::Sanitize.fragment(body, Sanitize::Config::RESTRICTED)
   end
 end
---
+++ app/models/mail/body.rb
@@ -0,0 +1,37 @@
+module Mail
+  class Body
+    def initialize(mail)
+      @mail = mail
+    end
+
+    def unparsed
+      mail.html_part.body
+    end
+
+    def text
+      mail.text_part&.body
+    end
+
+    def parsed
+      options = {
+        elements: %w[a p strong ul li ol img br b hr h1 h2 h3 h4],
+        attributes: {
+          'a' => %w[href title],
+          'img' => %w[alt src title width height]
+        },
+        protocols: {
+          'a' => {'href' => ['http', 'https', 'mailto']}
+        },
+        add_attributes: {
+          'a' => {'rel' => 'nofollow'}
+        }
+      }
+
+      ::Sanitize.fragment(unparsed, options)
+    end
+
+    def raw
+      ::Sanitize.fragment(unparsed, Sanitize::Config::RESTRICTED)
+    end
+  end
+end
