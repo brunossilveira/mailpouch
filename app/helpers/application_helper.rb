f5e33db35a657e0c204ae0703f75a5cc55d96fd4
Bruno Silveira <djbrunosilveira@gmail.com>
2022-12-13 22:10:29 -0300

    Adds fallback header for unavatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def unavatar_url(domain)
+    "https://unavatar.io/#{domain}?fallback=https://source.boringavatars.com/pixel/42/2?colors=D95534,F0B17D,EDDD9F,1FB6CC,9AD4D3"
+  end
+
   def unsubscribe_alert
     if current_user && !current_user.subscribed?
       "#{checkout_link} so you don't miss any newsletters!".html_safe
