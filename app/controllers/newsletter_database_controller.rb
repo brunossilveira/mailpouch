5cd880e981ff7930eb13b9b0517acd44017f565e
Bruno Silveira <djbrunosilveira@gmail.com>
2023-04-07 10:55:18 -0300

    Fixes newsletter_database controller

--- app/controllers/newsletter_database_controller.rb
+++ app/controllers/newsletter_database_controller.rb
@@ -1,6 +1,6 @@
 class NewsletterDatabaseController < ApplicationController
   def index
-    @categories = Newsletter.pluck(:category).map(&:downcase).uniq.compact
+    @categories = Newsletter.pluck(:category).compact.map(&:downcase).uniq
     @category = params[:category] || 'startups'
 
     @newsletters = Newsletter.where(category: [@category.downcase, @category.humanize])
