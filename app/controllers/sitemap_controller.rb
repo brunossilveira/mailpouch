4bca7e192bab33541140fa0558affa0f75dd9d1f
Bruno Silveira <djbrunosilveira@gmail.com>
2023-07-17 22:03:43 -0300

    More URLs to sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,5 +2,6 @@ class SitemapController < ApplicationController
   def index
     @host = "https://mailpouch.app"
     @posts = Post.published
+    @categories = Newsletter.pluck(:category).compact.map(&:downcase).uniq
   end
 end
